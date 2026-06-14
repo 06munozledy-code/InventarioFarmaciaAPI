bb39b61f60d5cd6a311c15ff9e17ba383ed5ca0a
06munozledy-code <06munozledy@gmail.com>
2026-06-14 02:02:57 -0400

    Implementa controladores del sistema #10

---
+++ Controllers/MedicamentosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioFarmaciaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MedicamentosController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObtenerMedicamentos()
+        {
+            return Ok("Lista de medicamentos");
+        }
+    }
+}
