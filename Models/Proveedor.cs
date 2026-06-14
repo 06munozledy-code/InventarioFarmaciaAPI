1b63b10492dff08c88af66bed693a68114e1d348
06munozledy-code <06munozledy@gmail.com>
2026-06-14 01:59:58 -0400

    Implementa entidades del dominio #7

---
+++ Models/Proveedor.cs
@@ -0,0 +1,9 @@
+﻿namespace InventarioFarmaciaAPI.Models
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+    }
+}
